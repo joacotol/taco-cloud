cab9e46b99147188ded7ff134232a07e0d2772a0
joacotol <joacochrist@gmail.com>
2023-10-14 21:25:38 -0400

    Create a test file for the home controller

---
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package sia.tacocloud;
+import static org.hamcrest.Matchers.containsString;
+import static
+        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class) // web test for HomeController
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc; // injects MockMvc
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/")) // performs GET
+                .andExpect(status().isOk()) // expects HTTP 200
+                .andExpect(view().name("home")) // expects home view
+                .andExpect(content().string(containsString("Welcome to..."))); // expects welcome to
+
+    }
+}
